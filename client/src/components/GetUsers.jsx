7067aaa35e42beb4d3cd185f80adef6dd9aa9c90
rahul <rahul.jain.0681@gmail.com>
2024-01-27 22:04:12 +0530

    UI updated

--- client/src/components/GetUsers.jsx
+++ client/src/components/GetUsers.jsx
@@ -7,23 +7,28 @@ import { NavLink } from 'react-router-dom'
 import axios from 'axios';
 import { toast } from 'react-toastify';
 
+import { Oval } from 'react-loader-spinner';
 
 
 const GetUsers = () => {
 	const [users, setUsers] = useState([]);
 	const [isEmpty,setIsEmpty] = useState(false);
+	const [flag,setFlag] = useState(true);
 
 
 	useEffect(() => {
+		setFlag(true);
 		async function getData() {
 			try{
 				const resp = await axios.get("https://mern-crud-backend-ep14.onrender.com/getall")
 				// console.log(resp.data);
 				setUsers(resp.data);
+				setFlag(false);
 			}
 			catch(err){
 				// console.log(err.response);
 				setIsEmpty(true);
+				setFlag(false);
 				toast.error(err.response.data.msg);
 			}
 		}
@@ -59,53 +64,66 @@ const GetUsers = () => {
 
 	return (
 		<>
-			<div className="main-container">
-				<NavLink to={"/create"} className="add-btn">Add user</NavLink>
+			{
+				flag?
+				
+				<div className="oval">
+					<h1>Fetching Data</h1>
+					<Oval width={60} color="#0a4a0f"/>
+				</div>:
+				
 
-				{
-					isEmpty?
-					(
-						<h1 className='not-found-header'>User Data not found</h1>
-					):
+				<div className="main-container">
+					<NavLink to={"/create"} className="add-btn">Add user</NavLink>
+
+					{
+						isEmpty?
+						(
+							<h1 className='not-found-header'>User Data not found</h1>
+						):
+						
+
+
+						(
+							<table border={2}>
+								<thead>
+									<tr>
+										<th>S.No</th>
+										<th>Name</th>
+										<th>Email</th>
+										<th>Action</th>
+									</tr>
+								</thead>
+
+								<tbody>
+
+									{
+										users.map((element, index) => {
+											return (
+												<tr key={index}>
+													<td>{index+1}</td>
+													<td>{element.firstName} {element.lastName}</td>
+													<td>{element.email}</td>
+													<td className='action-btns'>
+														<button className='btn-1' onClick={()=>handleDelete(element._id)}><DeleteIcon fontSize="inherit" /></button>
+														<button className='btn-2'><NavLink to={`/updateone/${element._id}`}><EditIcon fontSize="inherit" /></NavLink></button>
+													</td>
+												</tr>
+											)
+										})
+									}
+								</tbody>
+							</table>
+						)
+
+					}
 					
+					
+				</div>
+
+			}
 
 
-					(
-						<table border={2}>
-							<thead>
-								<tr>
-									<th>S.No</th>
-									<th>Name</th>
-									<th>Email</th>
-									<th>Action</th>
-								</tr>
-							</thead>
-
-							<tbody>
-
-								{
-									users.map((element, index) => {
-										return (
-											<tr key={index}>
-												<td>{index+1}</td>
-												<td>{element.firstName} {element.lastName}</td>
-												<td>{element.email}</td>
-												<td className='action-btns'>
-													<button className='btn-1' onClick={()=>handleDelete(element._id)}><DeleteIcon fontSize="inherit" /></button>
-													<button className='btn-2'><NavLink to={`/updateone/${element._id}`}><EditIcon fontSize="inherit" /></NavLink></button>
-												</td>
-											</tr>
-										)
-									})
-								}
-							</tbody>
-						</table>
-					)
-
-				}
-				
-				
-			</div>
 		</>
 	)
 }
